572ea3e237544941356873f836ee5c425049e62b
Jonny Frodsham <jonny.frodsham@havaslynx.com>
2015-11-05 15:57:53 +0000

    add vendor folder for modernizer and respond

--- application/tim-assets/themes/tatton/functions/javascripts.php
+++ application/tim-assets/themes/tatton/functions/javascripts.php
@@ -18,6 +18,9 @@ function ttEnqueueJs()
 {
 	if (!is_admin())
     {
+    	wp_register_script( 'modernizr-js', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js' );
+   		wp_enqueue_script( 'modernizr-js' );
+
     	wp_register_script('main-js', get_template_directory_uri() . '/assets/js/tatton.min.js', array('jquery'), '', filemtime(get_stylesheet_directory() . '/assets/js/tatton.min.js'), true);
     	wp_enqueue_script('main-js');
 	}
