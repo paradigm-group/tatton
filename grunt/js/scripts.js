dc06cd3557ff5bf8d01ffad0e92de624088368e5
Jonny Frodsham <jonny.frodsham@havaslynx.com>
2015-11-13 17:02:10 +0000

    craigs amends and add google marker

--- grunt/js/scripts.js
+++ grunt/js/scripts.js
@@ -77,9 +77,19 @@
                     ]);
 
                     var currentLocation = window.location.hostname,
+                        image = {
+                            url: 'http://'+currentLocation+'/tim-assets/themes/tatton/assets/img/marker.svg',
+                            // This marker is 20 pixels wide by 32 pixels high.
+                            size: new google.maps.Size(40, 66),
+                            // The origin for this image is (0, 0).
+                            origin: new google.maps.Point(0, 0),
+                            // The anchor for this image is the base
+                            anchor: new google.maps.Point(20, 66)
+                        },
                         marker = new google.maps.Marker({
                             position: myLatLng,
                             map: map,
+                            icon: image,
                             url : 'https://www.google.co.uk/maps/place/London+EC2N+1AR/@51.5145907,-0.0884503,17z/data=!3m1!4b1!4m2!3m1!1s0x48761cacd516d8b9:0xb2316b0662107b7b'
                         });
 
