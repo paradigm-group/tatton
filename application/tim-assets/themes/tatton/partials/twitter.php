b7966b49332631aeed6cb3daecfcb0a197ad0646
Jonny Frodsham <jonny.frodsham@havaslynx.com>
2016-01-14 12:15:15 +0000

    add twitter plugin and update twitter partial

--- application/tim-assets/themes/tatton/partials/twitter.php
+++ application/tim-assets/themes/tatton/partials/twitter.php
@@ -1,11 +1,21 @@
+<?php
+  $tweets = getTweets(1, 'TattonIM', array("exclude_replies" => true));
+  //var_dump($tweets);
+
+  foreach($tweets as $tweet):
+  	//var_dump($tweet['text']);
+?>
 <section class="twitter-block">
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-10 col-sm-offset-1 pt-lg pb-lg text-center">
 				<div class="twitter-block__icon twitter"></div>
-				<p class="twitter-block__tweet mt-md">Global car industry having their very own 'Libor' moment? Sector down -7.5% after VW emissions cheating admission.</p>
+				<p class="twitter-block__tweet mt-md"><?php echo $tweet['text']; ?></p>
             	<p><a class="more" href="<?php the_field('social_twitter', 'option'); ?>">More Twitter</a></p>
             </div>
         </div>
     </div>
-</section>
+</section> 	
+<?php
+  endforeach;
+?>
